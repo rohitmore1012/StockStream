2d931fdd6771c0db8d23d5dbbd409552ebdf0209
Rohit More <72143589+rohitmore1012@users.noreply.github.com>
2023-07-05 17:22:10 +0530

    Update StockStream.py

--- StockStream.py
+++ StockStream.py
@@ -44,8 +44,7 @@ if(selected == 'Stocks Performance Comparison'):  # if user selects 'Stocks Perf
         time.sleep(2)
         # st.success('Loaded')
 
-    dict_csv = pd.read_csv('StockStreamTickersData.csv', header=None,
-                           index_col=0, squeeze=True).to_dict()  # read csv file
+    dict_csv = pd.read_csv('StockStreamTickersData.csv', header=None, index_col=0).to_dict()[1]  # read csv file
     symb_list = []  # list for storing symbols
     for i in dropdown:  # for each asset selected
         val = dict_csv.get(i)  # get symbol from csv file
@@ -133,8 +132,7 @@ elif(selected == 'Real-Time Stock Price'):  # if user selects 'Real-Time Stock P
     with st.spinner('Loading...'):  # spinner while loading
             time.sleep(2)
 
-    dict_csv = pd.read_csv('StockStreamTickersData.csv', header=None,
-                           index_col=0, squeeze=True).to_dict()  # read csv file
+    dict_csv = pd.read_csv('StockStreamTickersData.csv', header=None, index_col=0).to_dict()[1]  # read csv file
     symb_list = []  # list for storing symbols
 
     val = dict_csv.get(a)  # get symbol from csv file
@@ -208,8 +206,7 @@ elif(selected == 'Stock Prediction'):  # if user selects 'Stock Prediction'
     a = st.selectbox('Pick a Company', tickers)
     with st.spinner('Loading...'):  # spinner while loading
              time.sleep(2)
-    dict_csv = pd.read_csv('StockStreamTickersData.csv', header=None,
-                           index_col=0, squeeze=True).to_dict()  # read csv file
+    dict_csv = pd.read_csv('StockStreamTickersData.csv', header=None, index_col=0).to_dict()[1]  # read csv file
     symb_list = []  # list for storing symbols
     val = dict_csv.get(a)  # get symbol from csv file
     symb_list.append(val)  # append symbol to list
@@ -274,13 +271,6 @@ elif(selected == 'About'):
     </style>
     """, unsafe_allow_html=True)
     
-    # st.markdown("""
-    # StockStream is a web application that allows users to view Real-Time Stock Prices, Stock Price Prediction, and Stock Price Analysis. 
-    # The application is developed using Python, Plotly, and Streamlit.
-    # Its developed by Vaishnavi Sharma and Rohit More.
-    # Its code is available on GitHub. 
-    # Its built on top of Streamlit, a Python framework for building web applications, Plotly, a Python library for creating charts, Yahoo Finance API, a Python library for fetching stock data, Prophet, a Python library for predicting stock prices.""")
-
     st.markdown('<p class="big-font">StockStream is a web application that allows users to visualize Stock Performance Comparison, Real-Time Stock Prices and Stock Price Prediction. This application is developed using Streamlit. Streamlit is an open source app framework in Python language. It helps users to create web apps for Data Science and Machine Learning in a short time. This Project is developed by Vaishnavi Sharma and Rohit More. You can find more about the developers on their GitHub Profiles shared below.<br>Hope you are able to employ this application well and get your desired output.<br> Cheers!</p>', unsafe_allow_html=True)
     st.subheader('Rohit More [![Repo](https://badgen.net/badge/icon/GitHub?icon=github&label)](https://github.com/rohitmore1012) ')
     st.subheader('Vaishnavi Sharma [![Repo](https://badgen.net/badge/icon/GitHub?icon=github&label)](https://github.com/vaishnavi3131) ')
